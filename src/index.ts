78838e0d2d2d62b4c647ea3b86549344e4eb5035
Alex Stelea <alex.s@rdx.works>
2024-03-21 12:20:00 +0000

    add redis client

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
+import { createClient } from "redis";
+import Pino from "pino";
+
+const logger = Pino({ level: "debug" });
+
+const client = await createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+})
+  .on("error", (err) => logger.error("Redis Client Error", err))
+  .connect();
+
+logger.debug("Redis Client Started!");
+
+await client.set("foo", "bar");
+
+await client.disconnect();
+
+logger.debug(`Server running on port: ${process.env.PORT}`);
+
 Bun.serve({
   port: process.env.PORT,
-  fetch(req) {
-    return new Response("Bun!");
+  fetch: async (req) => {
+    const value = await client.get("key");
+    return new Response(`got '${value}' from Redis`);
   },
 });
