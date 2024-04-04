fdb325deca5a1626d0b74610622a598a6faf6ca5
Dawid Sowa <dawid.sowa@rdx.works>
2024-04-04 15:07:42 +0200

    fix: allow CORS

--- src/router/helpers/routeHandler.ts
+++ src/router/helpers/routeHandler.ts
@@ -13,6 +13,8 @@ export const routeHandler = ({
     status: status,
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST",
     },
   });
 };
